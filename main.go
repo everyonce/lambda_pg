cadf5afcf2be09d0e1dc11acadbf28ac061e2202
daniel.ness <daniel.ness@cgi.com>
2018-05-18 16:27:27 -0500

    it works

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 
 import (
 	"database/sql"
-	_ "fmt"
+	"fmt"
 	_ "io"
 	"log"
 	"os"
+	"bytes"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/joho/sqltocsv"
 	_ "github.com/lib/pq"
 )
@@ -26,8 +30,30 @@ func hello() (string, error) {
 		log.Fatal(err)
 	}
 	defer rows.Close()
+	var writer bytes.Buffer
+	err = sqltocsv.Write(&writer, rows)
+	//	os.Stdout, rows)
 
-	err = sqltocsv.Write(os.Stdout, rows)
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	}))
+	uploader := s3manager.NewUploader(sess)
+	//f, err  := os.Open(filename)
+
+// Upload the file to S3.
+result, err := uploader.Upload(&s3manager.UploadInput{
+    Bucket: aws.String("dness-temp"),
+    Key:    aws.String("csv_from_fable.csv"),
+    Body:   &writer,
+})
+if err != nil {
+    panic(err)
+}
+fmt.Println(result)
+
+
+	
 	if err != nil {
 		panic(err)
 	}
